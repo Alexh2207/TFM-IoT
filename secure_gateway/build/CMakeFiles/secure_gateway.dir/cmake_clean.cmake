865be876cc1fc6d55632b5efd20a3bb1158c93aa
Alexh23 <a.hontanillab@alumnos.upm>
2024-10-27 18:28:02 +0100

    First commit

---
+++ secure_gateway/build/CMakeFiles/secure_gateway.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/secure_gateway"
+  "../bin/secure_gateway.pdb"
+  "CMakeFiles/secure_gateway.dir/src/secure_gateway.cpp.o"
+  "CMakeFiles/secure_gateway.dir/src/secure_gateway.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/secure_gateway.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
